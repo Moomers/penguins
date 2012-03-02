0bf4a54e54468aeeb9d8825e33d53301fe97ad3b
Igor Serebryany <igor47@moomers.org>
2012-03-02 03:29:04 -0800

    add a test and more stuff to make it seem like a module

---
+++ socket/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+left = '3000-6A06-3142-3732-7346-2543'
+right = '3000-6F06-3142-3732-4454-2543'
+smcpath = '/root/pololu/smc_linux/SmcCmd'
+
+from driver.smccmd import SmcCmdDriver
+d = SmcCmdDriver(left, right, smcpath)
+print "speeds -- left: %s\tright: %s" % (d.left, d.right)
+
