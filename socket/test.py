31366af0347f63df93ee2fbd8a7d2777167f50fe
Igor Serebryany <igor47@moomers.org>
2012-03-02 03:49:38 -0800

    fix typo in test script

--- socket/test.py
+++ socket/test.py
@@ -4,7 +4,7 @@ left = '3000-6A06-3142-3732-7346-2543'
 right = '3000-6F06-3142-3732-4454-2543'
 smcpath = '/root/pololu/smc_linux/SmcCmd'
 
-from driver.smccmd import SmcCmdDriver
+from drivers.smccmd import SmcCmdDriver
 d = SmcCmdDriver(left, right, smcpath)
 print "speeds -- left: %s\tright: %s" % (d.left, d.right)
 
