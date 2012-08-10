bf484423337b492870330af69fa4850afb6da331
Igor Serebryany <igor47@moomers.org>
2012-08-10 16:15:11 -0700

    avoid busy loop; a status property for sending to clients

--- server/monitor.py
+++ server/monitor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import time
 import threading
 
 class ServerMonitor(threading.Thread):
@@ -14,7 +15,7 @@ class ServerMonitor(threading.Thread):
         """Polls for state and sends a heartbeat."""
         # Run until told to stop.
         while not self._stop.isSet():
-            pass
+            time.sleep(.1)
 
         # remove reference to the arduino (for GC)
         self.arduino = None
@@ -22,3 +23,13 @@ class ServerMonitor(threading.Thread):
     def stop(self):
         """Signals that the monitor thread should stop."""
         self._stop.set()
+
+    @property
+    def status(self):
+        """Returns the monitor status"""
+        status = {
+                'client_age':(time.time() - self.server.last_request ),
+                }
+        return status
+
+
