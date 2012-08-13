40a56a765bb42afb95f367419ece6427c62037b9
Igor Serebryany <igor47@moomers.org>
2012-08-13 14:23:54 -0700

    remove obsolete non-conforming drivers; might re-add later to add support for 'em

--- server/drivers/__init__.py
+++ server/drivers/__init__.py
@@ -3,8 +3,5 @@
 import smcserial, smcstub, smccmd, sabertooth
 
 driverlist = {
-        'smccmd':('Runs SmcCmd to process every request', smccmd),
-        'smcserial':('Drives controllers using the serial protocol via a tty interface', smcserial),
-        'smcstub':('Stub test driver for SMC controller-based drivers', smcstub),
         'sabertooth':('Talks to the Sabertooth 2x60 via the on-board arduino', sabertooth),
         }
