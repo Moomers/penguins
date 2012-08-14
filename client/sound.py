6ee7ab9a06271a167e6b9b0edf7637a98bc1d976
Igor Serebryany <igor47@moomers.org>
2012-08-13 17:02:31 -0700

    rearrange joyride into it's own submodule; factor out the sound code into it's own file in the top-level module

---
+++ client/sound.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""Functionality to play sounds in response to events"""
+
+import os
+import pyaudio
+import Queue
+import threading
+import wave
+
+SOUND_DIR = '../../sounds/joyride'
+
+class Sound(object):
+    def __init__(self, name):
+        self.wf = wave.open(os.path.join(SOUND_DIR, name + '.wav'), 'rb')
+
+SOUNDS = dict((name, Sound(name))
+              for name in ['ebrake', 'honk', 'yay', 'screech',
+                           'tada', 'uhoh', 'smb_bump', 'smb_warning',
+                           'hothothot', 'relief'])
+
+class Mixer(threading.Thread):
+    CHUNKSIZE = 1024
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
+        self._queue = Queue.Queue(0)
+        self.pyaudio = pyaudio.PyAudio()
+        self._stop = threading.Event()
+
+    def stop(self):
+        self._stop.set()
+        self.pyaudio.terminate()
+
+    def run(self):
+        while not self._stop.isSet():
+            try:
+                sound = self._queue.get(block=False, timeout=0.1)
+                self._play(sound)
+            except Queue.Empty:
+                pass
+
+    def queue(self, sound):
+        self._queue.put(sound)
+
+    def _play(self, sound):
+        stream = self.pyaudio.open(
+            format=self.pyaudio.get_format_from_width(sound.wf.getsampwidth()),
+            channels=sound.wf.getnchannels(),
+            rate=sound.wf.getframerate(),
+            output=True)
+        stream.start_stream()
+        sound.wf.rewind()
+        data = sound.wf.readframes(Mixer.CHUNKSIZE)
+        while data != '':
+            stream.write(data)
+            data = sound.wf.readframes(Mixer.CHUNKSIZE)
+        stream.close()
+
+if __name__ == "__main__":
+    mixer = Mixer()
+    mixer.start()
+    mixer.queue(SOUNDS['tada'])
+    mixer.stop()
