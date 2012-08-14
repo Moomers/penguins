3d5d97221a5be4cf85bf179ea3e5b4d44893117b
Igor Serebryany <igor47@moomers.org>
2012-08-13 17:04:58 -0700

    remove useless inputs submodule; it's not going to work

--- client/inputs/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import joystick, keyboard
-
-inputlist = {
-        'nes':{'description':'An NES controller', 'device':lambda device, **rest: joystick.Joystick(device, NESController())},
-        'keyboard':{'description':'The keyboard arrow keys', 'device':lambda **rest: keyboard.Keyboard()},
-        }
