4005b22f93f59af73b905d511b74bc0bb7ca8a23
Jered Wierzbicki <jeredw@gmail.com>
2012-08-14 19:40:00 -0700

    fix test in braking logic

--- server/drivers/sabertooth.py
+++ server/drivers/sabertooth.py
@@ -116,7 +116,9 @@ class SabertoothDriver(object):
         for i in (0, 1):
             # what is our maximum acceleration speed?
             # if we're actively braking, we can change at up to brake speed
-            if self.braking_speed and abs(target_speeds[i]) < self.last_speeds[i]:
+            # never use braking to accelerate though; in that case, use
+            # normal acceleration model
+            if self.braking_speed and abs(target_speeds[i]) < abs(self.last_speeds[i]):
                 max_diff = self.braking_speed
             # otherwise we're accelerating/decellerating normally
             else:
